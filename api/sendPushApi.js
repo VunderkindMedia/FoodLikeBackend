a54e1db0fb8c1f839dba154d28e3a11b1de3cda3
VunderkindMedia <idstrelov@okha65.ru>
2021-07-17 15:00:56 +1100

    Редактирование пуш уведомлений

--- api/sendPushApi.js
+++ api/sendPushApi.js
@@ -7,19 +7,19 @@ const sendPushApi = async ({title, description, users_list}) => {
       notification: {
         body: description,
         title: title,
-        sound: 'default',
-        },
-        data: {
-          title: title,
-          message: description
-        }
+        sound: 'default'
+      },
+      data: {
+        title: title,
+        message: description
+      }
     }
     console.log(JSON.stringify(form));
     fetch('https://fcm.googleapis.com/fcm/send', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'EcNEM1PFSQpqpFOy4kS3QxQ:APA91bEJk4CTfRBede0dd9t6u05tUrD19Soc5b1w6nhC678J9WE-qlQflAd9t-Fgv8FY_Ps_L6qN1oOYHfAetO_4srkn3zv4RtBP2ES7_SrH4j1PFrVRLBVhzZ1jPPdJr8YQ7pXyndMB'
+        'Authorization': 'key=EcNEM1PFSQpqpFOy4kS3QxQ:APA91bEJk4CTfRBede0dd9t6u05tUrD19Soc5b1w6nhC678J9WE-qlQflAd9t-Fgv8FY_Ps_L6qN1oOYHfAetO_4srkn3zv4RtBP2ES7_SrH4j1PFrVRLBVhzZ1jPPdJr8YQ7pXyndMB'
       },
       body: JSON.stringify(form)
     })
